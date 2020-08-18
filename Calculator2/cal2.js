aaddba2c4c98d198f09306ea6b40e66e98bbad4f
JeffyMahin <jeffymahin@gmail.com>
2020-08-18 17:14:26 +0600

    first commit

---
+++ Calculator2/cal2.js
@@ -0,0 +1,64 @@
+function clickNum(n) {
+    var existingNumber= $('#result').val()
+    $('#result').val(existingNumber + n)
+
+}
+
+function clearResult() {
+    $('#result').val('')
+
+}
+
+function deleteNumber() {
+    var exist= $('#result').val()
+    $('#result').val(exist.slice(0,-1))
+}
+
+function calculate() {
+    var result= eval($('#result').val())
+    $('#result').val(result)
+}
+
+function sqrtNum() {
+    var exist= $('#result').val()
+    $('#result').val(Math.sqrt(exist))
+}
+
+function sinVal() {
+     var exist= $('#result').val()
+    $('#result').val(Math.sin(exist))
+}
+function cosVal() {
+     var exist= $('#result').val()
+    $('#result').val(Math.cos(exist))
+}
+function tanVal() {
+     var exist= $('#result').val()
+    $('#result').val(Math.tan(exist))
+}
+function squareNum() {
+    var exist= $('#result').val()
+    $('#result').val(exist**2)
+}
+
+function factorialNum() {
+    var present= $('#result').val();
+    var output = 1;
+    if(present <= 0){
+			 output = 1;
+		}else{
+			for(i=1; i<=present; i++){
+				output = output*i;
+			}
+		}
+		$("#result").val(output);
+}
+
+function expNum() {
+    var present= $('#result').val();
+    $('#result').val(Math.exp(present));
+}
+
+function lnFun() {
+     $('#result').val(Math.log($('#result').val()));
+}
